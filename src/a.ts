ea35704b5055956c731a958214f625def19424f1
Reuven, Chen <Chen.Reuven@emc.com>
2019-03-21 19:31:25 +0200

    add tslint and typesscript

---
+++ src/a.ts
@@ -0,0 +1,9 @@
+import { B } from "./b";
+
+class A {
+
+    public getB(): B {
+        const bb = new B();
+        return bb;
+    }
+}
---
+++ src/b.ts
@@ -0,0 +1,5 @@
+export class B {
+    public getGreeting() {
+        return "hello";
+    }
+}
